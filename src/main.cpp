07e41b2cad61f1583c3ac92ae67b0c36e09d8ff2
AruulmozhivarmanS <aruulmozhivarman@outlook.com>
2017-10-22 00:06:41 +0530

    Fixed error

--- src/main.cpp
+++ src/main.cpp
@@ -284,7 +284,7 @@ int main() {
 					double check_speed = sqrt(vx * vx + vy * vy);
 					double check_car_s = sensor_fusion[i][5];
 
-					check_car_s += ((double) prev_size * 0.2 * check_speed);
+					check_car_s += ((double) prev_size * 0.02 * check_speed);
 					
 					if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)){
 						too_close = true;
